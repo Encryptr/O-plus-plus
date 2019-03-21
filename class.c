2f65f3e0d1c4648115bc7bbe361c4fc5bb1e1df4
MadMax129 <38307091+MadMax129@users.noreply.github.com>
2019-03-20 20:42:39 -0700

    Add files via upload

--- class.c
+++ class.c
@@ -1,59 +1,75 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <string.h>
 
-#define MAX_LENGTH 100
+#define MAX_LENGTH 1000
 
-int idx = 0;
-char tok[MAX_LENGTH];
-
-typedef enum
+typedef enum 
 {
   IDLE,
+  STARTING,
   STARTED,
-  END,
-
+  ENDED,
 } States;
 
-States state = IDLE;
+char tokens[100][100] = {};
+int idx = 0;
 
-void lex(char *tok)
+int main(int argc, char *argv[])
 {
-  if (strcmp(tok, "class\n") == 0)
-  {
-    state = IDLE;
-  }
-
-  else if (strcmp(tok, "end\n") == 0)
-  {
-    state = END;
-  }
-
-  switch(state)
-  {
-    case IDLE:
-        state = STARTED;
-    break;
-
-    case STARTED:
-        
-    break;
-
-    case END:
-
-
-    break;
-
-  }
-
-}
-
-
-int main() 
-{
+  States state = IDLE;
   FILE *file;
-  file = fopen("text.txt", "r");
-  fgets(tok, MAX_LENGTH, file);
-  lex(tok);
+
+  file = fopen(argv[1], "rt");
+  if (file == NULL)
+  {
+    printf("NO FILE %s\n", argv[1]);
+    return -1;
+  }
+
+  char fline[255];
+  char tok[50];
+  
+  //Condition
+  const char *str_delim = " \n";
+
+  while (fgets(fline, MAX_LENGTH, file) != NULL)
+  {
+  	
+    char *sword = strtok(fline, str_delim);
+
+    while (sword != NULL)
+    {
+
+      if (strcmp(sword, "class") == 0)
+      {
+        state = STARTING;
+      }
+      else if (strcmp(sword, "end") == 0)
+      {
+        state = ENDED;
+      }
+
+      switch(state)
+      {
+        case IDLE:
+        break;
+
+        case STARTING:
+          state = STARTED;
+        break;
+
+        case STARTED:
+          strcpy(tokens[idx], sword);
+          idx++;
+        break;
+
+        case ENDED:
+          for (int i=0; i<idx; ++i) printf("%s\n", tokens[i]);
+        break;
+      }
+
+      sword = strtok(NULL, str_delim);
+    }
+  }
 
 }
